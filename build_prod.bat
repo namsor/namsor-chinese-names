0dad3387bb19d27c797a7d0d1e55395265e34a89
NamSor <elian.carsenat@namsor.com>
2019-06-18 14:49:54 +0200

    build
    build

--- build_prod.bat
+++ build_prod.bat
@@ -1,5 +1,6 @@
-REM ng build --prod --output-path docs --base-href /phone-validation/
-ng build --prod --output-path docs --base-href /
+REM WITHOUT CUSTOM DOMAIN ng build --prod --output-path docs --base-href /namsor-chinese-names/
+REM WITH CUSTOM DOMAIN ng build --prod --output-path docs --base-href /
+ng build --prod --output-path docs --base-href /namsor-chinese-names/
 copy docs\index.html docs\404.html
 copy src\robots.txt docs\robots.txt
 copy src\sitemap.xml docs\sitemap.xml
