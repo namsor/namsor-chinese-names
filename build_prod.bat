dc24062ad255fc04ee1df33e9908694f2c479c45
NamSor <elian.carsenat@namsor.com>
2019-06-20 11:59:36 +0200

    work
    work

--- build_prod.bat
+++ build_prod.bat
@@ -1,6 +1,6 @@
 REM WITHOUT CUSTOM DOMAIN ng build --prod --output-path docs --base-href /namsor-chinese-names/
 REM WITH CUSTOM DOMAIN ng build --prod --output-path docs --base-href /
-ng build --prod --output-path docs --base-href /namsor-chinese-names/
+ng build --prod --output-path docs --base-href /
 copy docs\index.html docs\404.html
 copy src\robots.txt docs\robots.txt
 copy src\sitemap.xml docs\sitemap.xml
