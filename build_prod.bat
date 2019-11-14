6f8ecaaad9ed09637c6d3f18ec03219e283192d0
NamSor <elian.carsenat@namsor.com>
2019-11-14 20:45:07 +0100

    added CN index
    added CN index

--- build_prod.bat
+++ build_prod.bat
@@ -4,4 +4,6 @@ ng build --prod --output-path docs --base-href /
 copy docs\index.html docs\404.html
 copy src\robots.txt docs\robots.txt
 copy src\sitemap.xml docs\sitemap.xml
+copy CNAME docs\CNAME
+
 REM you need to check that the deployment works and usually re-set Github Pages
